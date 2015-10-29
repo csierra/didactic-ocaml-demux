c5f35af8b3a7856e786c8f70b76a69c20c91a0ed
Carlos Sierra Andrés <csierra@bizonos.com>
2015-10-30 00:37:11 +0100

    PAT detection

--- main.ml
+++ main.ml
@@ -1,19 +1,40 @@
 open Bitstring
 
+let sync_channel_in channel = 
+  let rec loop_channel channel = 
+    match (input_byte channel) with
+     0x47 -> ()
+    | _ -> print_string "Skipping\n"; loop_channel channel in
+  loop_channel channel
+
 let line_stream_from_channel channel =
   let bytes = String.create 188 in
+    bytes.[0] <- Char.chr(0x47);
     Stream.from (
       (fun _ ->
          try 
-          really_input channel bytes 0 188;
-          Some (bytes)
+           sync_channel_in channel;
+           really_input channel bytes 1 187;
+           Some (bytes)
          with End_of_file -> None));;
+
+let print_pat = Bitstring.hexdump_bitstring stdout
+
+let print_packet = function
+  | 0 -> print_pat 
+  | _ -> fun _ -> ()
 
 let parse_packet packet =
   let bits = Bitstring.bitstring_of_string packet in 
     bitmatch bits with
-    | { synchro   : 8 ;
-        rest      : -1 : bitstring } -> print_string "packet\n"
+    | { 
+        _ : 8;
+        _ : 1;
+        _ : 1;
+        _ : 1;
+        pid : 13 : int;
+        payload      : -1 : bitstring 
+      } -> print_packet pid payload
     | { _ } -> failwith "not a packet"
 
 let main =  
