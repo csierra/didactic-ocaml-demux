211fe30bc22844da258d8a1f55052c2989f181c6
Carlos Sierra Andrés <csierra@bizonos.com>
2015-10-31 20:13:34 +0100

    prints pat apparently

--- main.ml
+++ main.ml
@@ -18,8 +18,57 @@ let line_stream_from_channel channel =
            Some (bytes)
          with End_of_file -> None));;
 
-let print_pat = Bitstring.hexdump_bitstring stdout
+let print_table table =
+    bitmatch table with 
+    | {
+        table_id                 :                  8;
+        section_syntax_indicator :                  1;
+        private_bit              :                  1;
+        reserved                 :                  2;
+        section_length_unused    :                  2;
+        section_length           :                 10;
+        tsid                     :                 16;
+        3                        :                  2;
+        version_number           :                  5;
+        current_next             :                  1;
+        section_number           :                  8;
+        last_section_number      :                  8;
+        table_data               : section_length * 8: bitstring;
+        crc32                    :                 32
+      } -> 
+          let rec consume_pat pat =
+              bitmatch pat with
+              | { 
+                  program_num : 16;
+                  7           :  3;
+                  pmt_pid     : 13
+                } -> 
+                  print_string ("PMT " ^ string_of_int(program_num) ^ " IN PID: " ^ string_of_int(pmt_pid) ^ "\n");
+                  consume_pat (Bitstring.dropbits 32 pat)
+              | { 0xFF : 8} -> 
+                  print_string ("END PAT?\n");
+              | { _ } -> 
+                  print_string ("Match failed: \n");
+                  Bitstring.hexdump_bitstring stdout pat;
+                  try 
+                      consume_pat (Bitstring.dropbits 32 pat)
+                  with Invalid_argument(argument) -> ()
 
+          in consume_pat table_data
+
+let print_pat pat = 
+    bitmatch pat with
+    | {
+        pointer_field :8;
+        pointer_filler_bytes : pointer_field * 8;
+        table : -1 : bitstring    
+      } when pointer_field > 0 -> print_table table
+    
+    | {
+        pointer_field : 8;
+        table : -1 : bitstring
+      } -> print_table table 
+  
 let print_packet = function
   | 0 -> print_pat 
   | _ -> fun _ -> ()
@@ -28,17 +77,21 @@ let parse_packet packet =
   let bits = Bitstring.bitstring_of_string packet in 
     bitmatch bits with
     | { 
-        _ : 8;
-        _ : 1;
-        _ : 1;
-        _ : 1;
-        pid : 13 : int;
-        payload      : -1 : bitstring 
+        (0x47)                                          : 8;
+        transport_error_indicator                       : 1;
+        payload_unit_start_indicator                    : 1;
+        transport_priority                              : 1;
+        pid                                             :13;
+        scrambling_control                              : 2; 
+        adaptation_field_exist                          : 1;
+        contains_payload                                : 1;
+        continuity_counter                              : 4;
+        payload                                         :-1:bitstring
       } -> print_packet pid payload
     | { _ } -> failwith "not a packet"
 
 let main =  
-  let ic = open_in_bin "football.ts" in
+  let ic = open_in_bin Sys.argv.(1) in
     try
       let stream = line_stream_from_channel ic in
         Stream.iter parse_packet stream;
